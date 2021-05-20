7bc5b9152033e772bcacd7e2672bcbe285b7a67d
abc <abc@example.com>
2021-05-20 09:49:49 +0530

    added git-demo project to local repo

---
+++ src/main/java/com/gitex/com/git_demo/App.java
@@ -0,0 +1,17 @@
+package com.gitex.com.git_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void adddata(String Data) {
+		System.out.println("print"+Data);
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
