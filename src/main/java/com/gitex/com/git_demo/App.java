6b218d18504faa49ec7e1a76c0a1c1571db17004
ankitd1997 <63224222+ankitd1997@users.noreply.github.com>
2021-05-24 23:28:00 +0530

    new methodad

--- src/main/java/com/gitex/com/git_demo/App.java
+++ src/main/java/com/gitex/com/git_demo/App.java
@@ -13,6 +13,12 @@ public class App
 			System.out.println("well");
 		}
 	}
+	
+	public void addmethod(){
+	System.out.println("well1");
+	}
+
+		
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
